a55dcae68c5bbb20ff2703cab4c010a88993357a
Sk. Jishan <119778588+SkJishan04@users.noreply.github.com>
2025-02-28 02:29:29 +0530

    adding config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Set the directory to watch
+WATCH_DIRECTORY = r"C:\Users\skjis\OneDrive\Desktop\Idea Lab Assessment\media"
+
+# Supported audio/video formats
+SUPPORTED_FORMATS = ('.mp3', '.wav', '.mp4', '.mkv', '.mov', '.flv', '.aac', '.m4a')
+
+# Path to store session data
+SESSION_FILE = "session.json"
